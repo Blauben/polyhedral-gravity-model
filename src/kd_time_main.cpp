7c7f3a3e0e37b483b5485a06b2b026177ab2061e
Ben Frauenknecht <ge47pos@tum.de>
2024-12-19 15:17:45 +0100

    Began feedback #8

--- src/kd_time_main.cpp
+++ src/kd_time_main.cpp
@@ -3,16 +3,16 @@
 
 namespace polyhedralGravity {
 
+    const std::vector<std::string> filePaths{"../example-config/data/Eros", "../example-config/data/Eros_scaled_1", "../example-config/data/Eros_scaled_2", "../example-config/data/Eros_scaled_3", "../example-config/data/Eros_scaled_4"};
+
     Polyhedron createBigPolyhedron(const PolyhedralSource &source, const PlaneSelectionAlgorithm::Algorithm &algorithm) {
         return Polyhedron(source, 1.0, NormalOrientation::OUTWARDS, PolyhedronIntegrity::HEAL, algorithm);
     }
 
     void BM_Polyhedron_Tree(benchmark::State &state, const PlaneSelectionAlgorithm::Algorithm &algorithm) {
-        const std::vector<std::string> configPaths{"../example-config/example_eros.yaml", "../example-config/example_eros_scaled_1.yaml", "../example-config/example_eros_scaled_2.yaml", "../example-config/example_eros_scaled_3.yaml", "../example-config/example_eros_scaled_4.yaml"};
-        const std::shared_ptr<ConfigSource> config = std::make_shared<YAMLConfigReader>(configPaths.at(state.range(0)));
-        const PolyhedralSource source = config->getDataSource()->getPolyhedralSource();
+        TetgenAdapter adapter{{filePaths.at(state.range(0)) + ".node", filePaths.at(state.range(0)) + ".face"}};
         for (auto _: state) {
-            Polyhedron polyhedron = createBigPolyhedron(source, algorithm);
+            Polyhedron polyhedron = createBigPolyhedron(adapter.getPolyhedralSource(), algorithm);
             benchmark::ClobberMemory();
         }
         state.SetComplexityN(static_cast<benchmark::ComplexityN>(std::get<1>(source).size()));
